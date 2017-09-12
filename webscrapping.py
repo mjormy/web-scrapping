17b0a7adb0259bcc6a2181bb46f51f0461dddc8a
Maria Jose Ormaza <mjormy.ec@gmail.com>
2017-09-12 16:08:29 -0500

    Extrae solo texto de los comentarios

--- webscrapping.py
+++ webscrapping.py
@@ -2,10 +2,11 @@ from bs4 import BeautifulSoup
 import re
 import urllib2
 
-url = r"html/5.html"
+url = r"html/4.html"
 soup = BeautifulSoup(open(url).read())
 
 comments = soup.find_all('div', {'class' : 'jive-comment-content clearfix'})
 
-for comment in comments:
+for comment_element in comments:
+	comment = comment_element.find('div', {'class': 'jive-rendered-content'}).getText()
 	print comment
